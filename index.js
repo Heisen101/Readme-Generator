07b59fa26d40ab123a07fb1377caef2c08f6ab7a
Vasile <vasileholding@gmail.com>
2023-09-04 16:01:31 +0100

    added title, description section in generate readme function using vanila js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import fs from "fs";
 import inquirer from "inquirer";
 console.log(inquirer);
+
+function generateReadme(title, description) {
+  return `
+   # ${title}
+   ## Description
+   ${description}
+   
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)`;
+}
