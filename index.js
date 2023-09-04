6ddadb4e628c32bd0d3321e61eb42d286291311a
Vasile <vasileholding@gmail.com>
2023-09-04 16:16:09 +0100

    added section for each part in readme file to be created

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ import fs from "fs";
 import inquirer from "inquirer";
 console.log(inquirer);
 
-function generateReadme(title, description) {
+function generateReadme(
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contributing,
+  tests,
+  email
+) {
   return `
    # ${title}
    ## Description
@@ -15,5 +24,23 @@ function generateReadme(title, description) {
 - [License](#license)
 - [Contributing](#contributing)
 - [Tests](#tests)
-- [Questions](#questions)`;
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+This project is licensed under the ${license} license.
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+
+## Questions
+If you have any questions, you can reach me at ${email}. You can also check out my GitHub profile at [https://github.com/${githubUsername}](https://github.com/${githubUsername}).`;
 }
